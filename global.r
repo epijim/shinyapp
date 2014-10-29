6e1faa3767f14bba2382ba7710e085b0e34debdb
James B <james.black@cantab.net>
2014-10-29 22:23:27 +0000

    updates to allow guests not in groups be assigned

--- global.r
+++ global.r
@@ -6,18 +6,18 @@ library(shiny)
 
 ## Read in data
 pubs <- read.csv("data/pubs.csv", stringsAsFactors=F)  
-  pubs_all_list <- pubs$name
+pubs_all_list <- pubs$name
 google_distances <- read.csv("data/google_distances.csv", stringsAsFactors=F) 
 load("data/basemap_13.Rda")
 #load("data/basemap_14.Rda")
 
 ## Tidy data
 # Add merge variable to pubs
-  pubs$latlon <- paste0(pubs$lat,",",pubs$lon)
+pubs$latlon <- paste0(pubs$lat,",",pubs$lon)
 # Add google distances reversed to end
-  latlon_2 <- google_distances$latlon_1
-  latlon_1 <- google_distances$latlon_2
-  minutes <- google_distances$minutes
-  metres <- google_distances$metres
-  reversed <- data.frame(latlon_1,latlon_2,minutes,metres)
-  google_distances <- rbind(google_distances, reversed)
+latlon_2 <- google_distances$latlon_1
+latlon_1 <- google_distances$latlon_2
+minutes <- google_distances$minutes
+metres <- google_distances$metres
+reversed <- data.frame(latlon_1,latlon_2,minutes,metres)
+google_distances <- rbind(google_distances, reversed)
